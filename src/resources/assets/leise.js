3d9e833277a206dfffa1d8e27b1c2c4c620c976e
Thiemo Kunze <kunze@wangaz.com>
2016-04-16 00:43:36 +0200

    adding basic graph support

--- src/resources/assets/leise.js
+++ src/resources/assets/leise.js
@@ -1,25 +1,38 @@
-$(document).ajaxComplete(function()
-{
-	// get rounds
-	rounds = Array.apply(null, {length: ($('body#comproso form.cpage input[name="ccusr_rnd"]').val() + 1)}).map(Number.call, Number);
+$(document).on("updated", function () {
+	// slider
+	// user input
+	$('.leise.variable .extra.content .data.details input[type="range"]').on("input", function() {
+		$(this).parents(".extra.content").find('.statistic .value').html($(this).val());
+	});
 
-	// handle variables
-	$(".leise.variable").each(function() {
-		// create graph
-		var graphContainer = $(this).find(".graph.content");
-		var canvas = graphContainer.children("canvas");
-		canvas.html("");
+	// system input
+	$('.leise.variable .extra.content input').on("updated", function() {
+		$(this).parents(".extra.content").find('.statistic .value').html($(this).val());
+	});
 
-		var graph = new Chart(canvas.get(0).getContext("2d")).Line(
+	// graphs
+	// prepare round
+	round = $('body#comproso form.cpage input[name="ccusr_rnd"]').val();
+
+	// prepare graphs
+	graphs = {};
+
+	// set each graph
+	$(".leise.variable").each(function () {
+		// get canvas
+		canvas = $(this).find('.graph.content canvas');
+
+		// set graph
+		graphs[$(this).attr('id')] = new Chart(canvas.get(0).getContext("2d")).Line(
 			{
-				labels: rounds,
+				labels: [round],
 				datasets: [{
 					strokeColor: "rgba(220,220,220,1)",
-		            pointColor: "rgba(220,220,220,1)",
-		            pointStrokeColor: "#fff",
-		            pointHighlightFill: "#fff",
-		            pointHighlightStroke: "rgba(220,220,220,1)",
-		            data: $(this).find("input").data("value")
+					pointColor: "rgba(220,220,220,1)",
+					pointStrokeColor: "#fff",
+					pointHighlightFill: "#fff",
+					pointHighlightStroke: "rgba(220,220,220,1)",
+					data: $(this).find("input").val()
 				}]
 			},
 			{
@@ -29,19 +42,16 @@ $(document).ajaxComplete(function()
 				datasetFill: false
 			}
 		);
-		canvas.attr('width', graphContainer.width());
-		canvas.attr('height', 150);
+	});
+});
 
-		// slider
-		//$(this).find('.extra.content .data.details input[type="range"]').val($(this).find('.statistic .value').html());
-		$(this).find('.extra.content .data.details input[type="range"]').on("input", function() {
-			$(this).parents(".extra.content").find('.statistic .value').html($(this).val());
-		});
+$(document).on("ajaxProceeded", function()
+{
+	// set current round
+	round = $('body#comproso form.cpage input[name="ccusr_rnd"]').val();
 
-		//$(this).find('extra.content .statistic .value').html($(this).find('.extra.content input').val());
-
-		$(this).find('.extra.content input').on("updated", function() {
-			$(this).parents(".extra.content").find('.statistic .value').html($(this).val());
-		});
+	// update graphs
+	$.each(graphs, function (elementId, graph) {
+		graph.addData([$(elementId + 'input').val()], round);
 	});
 });
